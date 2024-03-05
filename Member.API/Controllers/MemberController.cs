1a0012b7b49b18c7262680735d9a9874fbf67122
Sameeh Hussein <sameehhussein02@gmail.com>
2024-03-05 19:54:59 +0200

    Configure Program.cs in API Add MemberController

---
+++ Member.API/Controllers/MemberController.cs
@@ -0,0 +1,27 @@
+﻿using Member.Application.Services.Interfaces;
+using Member.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Member.API.Controllers
+{
+    [Route("api/members")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly IMemberService _memberService;
+
+        public MemberController(IMemberService memberService)
+        {
+            _memberService = memberService ??
+                throw new ArgumentNullException(nameof(memberService));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberEntity>>> GetAllMembers()
+        {
+            var memberToReturn = await _memberService.GetAllMembers();
+            return Ok(memberToReturn);
+        }
+    }
+}
--- Member.API/Program.cs
+++ Member.API/Program.cs
@@ -1,3 +1,8 @@
+using Member.Application.Abstractions;
+using Member.Application.Services;
+using Member.Application.Services.Interfaces;
+using Member.DataAccess.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IMemberRepository, MemberRepository>();
+builder.Services.AddTransient<IMemberService, MemberService>();
 
 var app = builder.Build();
 
