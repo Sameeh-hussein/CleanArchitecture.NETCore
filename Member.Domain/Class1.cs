9c3994d47716102bdd51a3c13f3475ce30c861a6
Sameeh Hussein <sameehhussein02@gmail.com>
2024-03-05 16:39:18 +0200

    Add models to Domain layer

--- Member.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Member.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Member.Domain/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Member.Domain.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+    }
+}
